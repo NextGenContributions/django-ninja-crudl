c16045d0fd341627a6f398b68c944cefd596ff6e
Phuong Nguyen <phuong.fi.91@gmail.com>
2025-04-29 10:21:25 +0300

    chore: Explicitly mark uv as nox venv backend

--- noxfile.py
+++ noxfile.py
@@ -5,6 +5,9 @@ See: https://nox.thea.codes/en/stable/config.html
 
 import nox
 
+# Allow proper use of different python versions
+nox.options.default_venv_backend = "uv"
+
 
 @nox.session(python=["3.12", "3.13"], reuse_venv=True)
 def tests(session: nox.Session) -> None:
