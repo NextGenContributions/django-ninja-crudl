afd3329053c2268f6525f499e9d353409ad5d98d
Phuong Nguyen <phuong.fi.91@gmail.com>
2025-05-02 08:50:06 +0300

    chore: Fix tests

--- tests/test_openapi_schema_publishers.py
+++ tests/test_openapi_schema_publishers.py
@@ -2,6 +2,7 @@
 # pylint: disable=redefined-outer-name
 
 import json
+from pathlib import Path
 from typing import Any
 
 import pytest
@@ -13,7 +14,6 @@ from tests.utils import IntegerKeyJSONDecoder
 @pytest.fixture
 def openapi_schema() -> OpenAPISchema:
     """Get the OpenAPI schema."""
-    # return client.get("/api/openapi.json").json()
     from tests.test_django.urls import api
 
     return api.get_openapi_schema()
@@ -22,7 +22,8 @@ def openapi_schema() -> OpenAPISchema:
 @pytest.fixture
 def api_publishers() -> dict[str, Any]:  # pyright: ignore [reportExplicitAny]
     """Load a JSON fixture from the given file path."""
-    with open("tests/fixtures/api_publishers.json") as file:
+    fixture_path = Path(__file__).parent / "fixtures"
+    with open(fixture_path / "api_publishers.json") as file:
         return json.load(file, cls=IntegerKeyJSONDecoder)  # type: ignore[no-any-return]
 
 
